fe821232f24c877a6b17c4769f1523c37afdf80f
andre <andre.cunha.machado7@gmail.com>
2026-01-06 21:15:12 -0100

    no message

--- agrofieldtracknode/middleware/is_auth.js
+++ agrofieldtracknode/middleware/is_auth.js
@@ -5,36 +5,6 @@ const jwtkey = 'jkdoamnwpa';
 
 
 const verifyToken = async (req, res, next) => {
-    try {
-        let token = null;
-        // Verifica primeiro se o token vem no header (Bearer Token)
-        if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
-            token = req.headers.authorization.split(" ")[1];
-        }
-        // Caso contrário, tenta pegar do cookie
-        else {
-            const cookies = parse(req.headers.cookie || "");
-            token = cookies.auth;
-        }
-        // Se não houver token, retorna erro 401
-        if (!token) {
-            return res.status(401).json({ message: "Não autorizado. Faça login." });
-        } else {
-            // Verifica o token JWT
-            const decoded = jwt.verify(token, jwtkey);
-            const user = await Users.findById(decoded.user_id);
-            if (!user) {
-                return res.status(401).json({ message: "Utilizador não encontrado ou sessão expirada." });
-            } else {
-                // Define o usuário nos locals da resposta
-                res.locals.user = user; // Apenas o primeiro resultado
-                next(); // Continua para a próxima função na rota
-            }
-        }
-    } catch (error) {
-        console.error("Erro na autenticação:", error);
-        return res.status(401).json({ message: "Não autorizado ou sessão expirada." });
-    }
 
 }
 module.exports = verifyToken;
--- agrofieldtracknode/routes/MarketRoute.js
+++ agrofieldtracknode/routes/MarketRoute.js
@@ -6,13 +6,13 @@ const {upload, uploadPeixe} = require('../middleware/storage');
 
 const verifyToken = require("../middleware/is_auth");
 
-app.get('/comunidade',verifyToken, MarketController.getPublications);
-app.post('/insertcomunidade',verifyToken,upload.single('file'), MarketController.createPublication);
-app.get('/detalhescomunidade/:id',verifyToken, MarketController.getPublicationDetails);
-app.post('/deletecomunidade/:id',verifyToken, MarketController.deletePublication);
-app.post('/updatecomunidade/:id',verifyToken, upload.single('file'), MarketController.updatePublication);
-app.post('/comentario/:id',verifyToken, upload.array('file', 5), MarketController.addComentario);
-app.put('/comentarios/:id/editar',verifyToken,upload.array('file', 5), MarketController.editarComentario);
-app.delete('/comentarios/:id/remover',verifyToken, MarketController.removerComentario);
+app.get('/comunidade', MarketController.getPublications);
+app.post('/insertcomunidade',upload.single('file'), MarketController.createPublication);
+app.get('/detalhescomunidade/:id', MarketController.getPublicationDetails);
+app.post('/deletecomunidade/:id', MarketController.deletePublication);
+app.post('/updatecomunidade/:id', upload.single('file'), MarketController.updatePublication);
+app.post('/comentario/:id', upload.array('file', 5), MarketController.addComentario);
+app.put('/comentarios/:id/editar',upload.array('file', 5), MarketController.editarComentario);
+app.delete('/comentarios/:id/remover', MarketController.removerComentario);
 
 module.exports = app;
