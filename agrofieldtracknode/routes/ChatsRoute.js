a0c98a10ce32845392799c403be747866ea3ded4
andre <andre.cunha.machado7@gmail.com>
2026-01-07 00:23:26 -0100

    no message

---
+++ agrofieldtracknode/routes/ChatsRoute.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const ChatsController = require('../controllers/ChatsController');
+
+router.get('/chat/:user1_id/:user1_type/:user2_id/:user2_type', ChatsController.getOrCreateChat);
+module.exports = router;
+
