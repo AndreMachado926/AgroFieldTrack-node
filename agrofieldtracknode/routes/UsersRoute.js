d0c6305cc815ca0ee5afd0cd1ed249e96d75a17a
andre <andre.cunha.machado7@gmail.com>
2026-05-22 20:10:05 +0000

    no message

--- agrofieldtracknode/routes/UsersRoute.js
+++ agrofieldtracknode/routes/UsersRoute.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { getUserType } = require('../controllers/UsersController');
-const { isAuth } = require('../middleware/is_auth');
 
 // Rota para obter o tipo de utilizador
-router.get('/users/:userId/type', isAuth, getUserType);
+router.get('/users/:userId/type', getUserType);
 
 module.exports = router;
